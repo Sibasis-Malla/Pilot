2e1ca1f5768500975acf0b00566c896c9444fdae
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2022-06-15 02:38:38 +0530

    feat:sign up forms added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Marginals from './components/marginals/Marginals';
+import SignUp from './components/forms/SignUp';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Marginals>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </Marginals>
+    </Router>
   );
 }
 
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserCard from '../components/profile/ProfileCard';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <UserCard />
+    </div>
+  );
+};
+
+export default ProfilePage;
