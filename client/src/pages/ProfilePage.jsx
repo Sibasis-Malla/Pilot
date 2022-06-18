2bf2919a0d5ba4bbbb4781947bf352910f22c5a3
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2022-06-19 02:00:00 +0530

    chore/dependancy fix

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,20 +1,20 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogCard from '../components/profile/BlogCard';
 import UserCard from '../components/profile/ProfileCard';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
-import { getPublications } from "../Lens/query";
+import { getPublications } from '../Lens/query';
 import { useParams } from 'react-router-dom';
 const ProfilePage = () => {
-  const [data,setData] = useState([]);
-  const {id} = useParams()
-  useEffect(()=>{
-    handlePub()
-  },[])
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    handlePub();
+  }, []);
   const handlePub = async () => {
     const obj = {
       profileId: id,
-      publicationTypes: ["POST"],
+      publicationTypes: ['POST'],
       limit: 10,
     };
     const res = await getPublications(obj);
@@ -44,25 +44,23 @@ const ProfilePage = () => {
         All Blogs
       </Typography>
       <Grid container spacing={3} sx={{ mt: 1, mb: 10 }}>
-       { data && data.map((obj)=>{
-        const {metadata,id,createdAt} = obj
-        const date = new Date(createdAt)
-        //console.log(id);
-        //console.log(metadata.media[0].original.url);
-        return(
-          <BlogCard
-           title={metadata.name}
-           content = {metadata.content}
-           img = {metadata.media[0].original.url}
-           date = {String(date).slice(3,10)}
-           key = {id}
-           id = {id}
-          />
-        )
-
-       }
-      )}
-       
+        {data &&
+          data.map((obj) => {
+            const { metadata, id, createdAt } = obj;
+            const date = new Date(createdAt);
+            //console.log(id);
+            //console.log(metadata.media[0].original.url);
+            return (
+              <BlogCard
+                title={metadata.name}
+                content={metadata.content}
+                img={metadata.media[0].original.url}
+                date={String(date).slice(3, 10)}
+                key={id}
+                id={id}
+              />
+            );
+          })}
       </Grid>
     </div>
   );
