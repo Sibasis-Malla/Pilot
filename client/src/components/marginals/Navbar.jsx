3abfe403ac36925d0e5190cfa68ff08354d750c3
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2022-06-18 13:35:48 +0530

    fix:color fix of navbar

--- client/src/components/marginals/Navbar.jsx
+++ client/src/components/marginals/Navbar.jsx
@@ -10,19 +10,56 @@ import {
   Tooltip,
   IconButton,
   Avatar,
+  styled,
   Stack,
+  Badge,
 } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Link } from 'react-router-dom';
-import { Add, WindowRounded } from '@mui/icons-material';
+import { Add } from '@mui/icons-material';
 import Web3Context from '../../context';
 import RocketIcon from '@mui/icons-material/Rocket';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
 
 function Navbar() {
   const classes = useStyles();
   const { connectWallet, account, loginStat, login } = useContext(Web3Context);
   const [anchorElUser, setAnchorElUser] = useState(null);
   const settings = [
+    {
+      link: '/',
+      text: `Hey, ${String(account.currentAccount).slice(0, 5)}...${String(
+        account.currentAccount
+      ).slice(String(account.currentAccount).length - 5)}`,
+    },
     { link: '/profile', text: 'Profile' },
     { link: '/article/create', text: 'Write' },
     { link: '/', text: 'Sign Out' },
@@ -57,21 +94,68 @@ function Navbar() {
               {!loginStat ? (
                 <Button
                   variant="contained"
-                  style={{ backgroundColor: '#eebbc3', color: '#232946' }}
+                  style={{ color: '#fffffe', backgroundColor: '#7f5af0' }}
                   startIcon={<Add />}
                   onClick={() => login(account.currentAccount)}
                 >
-                  <Typography style={{ color: '#232946' }} variant="body1">
+                  <Typography style={{ color: '#fffffe' }} variant="body1">
                     Login to Lens
                   </Typography>
                 </Button>
               ) : (
-                <Typography className={classes.tab} variant="body1">
-                  Connected to Lens
-                </Typography>
+                <Stack
+                  direction="row"
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <div className={classes.ripple} />
+                  <Typography className={classes.tab}  variant="body1">
+                    Connected to Lens
+                  </Typography>
+                </Stack>
               )}
-
-              <Typography
+              <Box sx={{ flexGrow: 0, ml: 1 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Sambit Sankalp"
+                      src="https://res.cloudinary.com/tedxnitrourkela/image/upload/v1643281545/team/technical/Sambit_Sankalp_omdbgy.png"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map(({ link, text }) => (
+                    <Link
+                      style={{ textDecoration: 'none', color: '#000' }}
+                      key={text}
+                      to={link}
+                    >
+                      <MenuItem onClick={handleCloseUserMenu}>
+                        <Typography textAlign="center">{text}</Typography>
+                      </MenuItem>
+                    </Link>
+                  ))}
+                </Menu>
+              </Box>
+              {/* <Typography
                 className={classes.tab}
                 sx={{ ml: 2 }}
                 variant="body1"
@@ -80,16 +164,16 @@ function Navbar() {
                 {`${String(account.currentAccount).slice(0, 5)}...${String(
                   account.currentAccount
                 ).slice(String(account.currentAccount).length - 5)}`}
-              </Typography>
+              </Typography> */}
             </>
           ) : (
             <Button
               variant="contained"
-              style={{ backgroundColor: '#eebbc3', color: '#232946' }}
+              style={{ backgroundColor: '#7f5af0', color: '#fffffe' }}
               startIcon={<Add />}
               onClick={connectWallet}
             >
-              <Typography style={{ color: '#232946' }} variant="body1">
+              <Typography style={{ color: '#fffffe' }} variant="body1">
                 Connect to Wallet
               </Typography>
             </Button>
@@ -113,6 +197,14 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-between',
     alignItems: 'center',
     zIndex: 100,
+  },
+  ripple: {
+    width: '10px',
+    height: '10px',
+    borderRadius: '50%',
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    marginRight: '5px',
   },
   logoContainer: {
     textDecoration: 'none',
