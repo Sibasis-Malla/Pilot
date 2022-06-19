e9dd4e76f8a74c760218fe6d468d7005ea2b8cec
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2022-06-19 19:19:05 +0530

    fix:minor frontend changes

--- client/src/components/home/Trendings.jsx
+++ client/src/components/home/Trendings.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { CssBaseline, Grid, Typography } from '@mui/material';
 import Sidebar from './Sidebar';
-import BlogCard from '../profile/BlogCard';
 import SmallBlogCard from '../profile/SmallBlogCard';
 
 const Trendings = () => {
@@ -31,26 +30,6 @@ const Trendings = () => {
         <Grid item xs={12} md={4}>
           <Sidebar />
         </Grid>
-      </Grid>
-      <Typography
-        variant="h4"
-        sx={{
-          textAlign: 'left',
-          fontSize: '2rem',
-          fontWeight: 'bold',
-          mt: 3,
-          color: '#fffffe',
-        }}
-      >
-        Recent Articles
-      </Typography>
-      <Grid container spacing={3} sx={{ mt: 0.5, mb: 5 }}>
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
       </Grid>
     </>
   );
